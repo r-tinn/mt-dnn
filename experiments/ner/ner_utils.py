34387aebb49115b70b1fa83a0bb3b7932b8df2b7
Robert Tinn <t-rotinn@microsoft.com>
2020-02-23 13:58:18 +0000

    Add in function loading ner iob files

--- experiments/ner/ner_utils.py
+++ experiments/ner/ner_utils.py
@@ -3,6 +3,42 @@ from sys import path
 path.append(os.getcwd())
 from data_utils.task_def import DataFormat
 
+"""
+Load IOB tsv file:
+
+Immunohistochemical	O
+staining	O
+was	O
+positive	O
+for	O
+S	B
+-	I
+100	I
+"""
+def load_ner_tsv(
+    tsv_file: str,
+):
+    rows = []
+    with open(tsv_file) as tsvfile:
+        tsvreader = csv.reader(tsvfile, delimiter="\t", quotechar=None)        
+        cnt = 0
+        sentence = []
+        label = []
+        for line in tsvreader:
+            if not line:
+                sample = {'uid': cnt, 'premise': sentence, 'label': label}
+                rows.append(sample)
+                sentence = []
+                label = []
+                cnt += 1
+                continue
+            sentence.append(line[0])
+            iob_label = line[1]
+            if iob_label == 'B' or iob_label == 'I':
+                iob_label += '-MISC'
+            label.append(iob_label)
+    return rows
+
 def load_conll_ner(file, is_train=True):
     rows = []
     cnt = 0
